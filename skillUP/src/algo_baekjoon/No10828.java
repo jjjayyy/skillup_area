db70196ce2c8a23db2e52ee4654b346b40e83162
jay <hwinnovation@navaer.com>
2018-11-07 14:59:16 +0900

    ing

--- skillUP/src/algo_baekjoon/No10828.java
+++ skillUP/src/algo_baekjoon/No10828.java
@@ -13,33 +13,36 @@ public class No10828 {
 	 * size: 스택에 들어있는 정수의 개수를 출력한다.
 	 * empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
 	 * top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-	 */	
-	
-	
-	
+	 */
+
+
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int N = Integer.parseInt(br.readLine());
-		String[] order = null;
-		int printNum = 0;;
-		
+		String[] order = new String[N];
+		int printNum = 0;
+
 		for(int i = 0; i < N; i++) {
 			order[i] = br.readLine();
 		}
-		
+
 		bw.write(Integer.toString(printNum));
 		bw.flush();
 		bw.close();
 	}
-	
+
+	int cnt = 0;
 	int[] stack = null;
-	
-	public void push(int i, int stackElem) {
-		stack[i] = stackElem;
+
+	public void push(int stackElem) {
+		stack = new int[top()];
+			stack[cnt] = stackElem;
+			cnt++;
 	}
-	
+
 	public int pop() {
 		if(empty() == 1) {
 			return -1;
@@ -48,23 +51,23 @@ public class No10828 {
 		stack[stack.length-1] = 0;
 		return num;
 	}
-	
+
 	public int size() {
 		return stack.length;
 	}
-	
+
 	public int empty() {
 		if(stack.length==0) {
 			return 1;
 		}
 		return 0;
 	}
-	
+
 	public int top() {
 		if(empty() == 1) {
 			return -1;
 		}
 		return stack[stack.length - 1];
 	}
-	
+
 }
