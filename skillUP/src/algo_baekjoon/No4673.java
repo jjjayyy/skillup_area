c88085c3302fce26eaa57f06fa54f002a2c36b8b
LJH\JH <hwinnovation@naver.com>
2018-11-11 23:03:21 +0900

    Self Number update

--- skillUP/src/algo_baekjoon/No4673.java
+++ skillUP/src/algo_baekjoon/No4673.java
@@ -3,30 +3,29 @@ package algo_baekjoon;
 public class No4673 {
 
 	public static void main(String[] args) {
-		
 		boolean[] isNotSelfNum = new boolean[10001];
-		
-		for(int i = 0; i < 10000; i++) {
-			int snum = selfNum(i);
-			if(snum <= 10000) {
-				isNotSelfNum[snum] = true;
+		for(int i = 1; i <= 10000; i++) {
+			if(dn(i) <= 10000) {
+				isNotSelfNum[dn(i)] = true;
 			}
 		}
 		
-		for(int i = 1; i<isNotSelfNum.length; i++) {
-			if(!isNotSelfNum[i]) {
-				System.out.println(i);
+		for(int j = 1; j <= 10000; j++) {
+			if(!isNotSelfNum[j]) {
+				System.out.println(j);
 			}
 		}
+	}
+
+	public static int dn(int n) {
+		int dn = n;
+			
+		while(n > 0) {
+			dn += n % 10;
+			n /= 10;
+		}
+		
+		return dn;
 	}
 	
-	public static int selfNum(int num) {
-		
-		int snum = num;
-		while(num > 0) {
-			snum += num % 10;
-			num /= 10;
-		}
-		return snum;
-	}
 }
