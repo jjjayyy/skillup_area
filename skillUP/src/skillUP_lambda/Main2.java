64f7642faf9bc0f484cc546cc3dae51e7264fb41
LJH\JH <hwinnovation@naver.com>
2018-11-22 23:04:16 +0900

    lambda continue

---
+++ skillUP/src/skillUP_lambda/Main2.java
@@ -0,0 +1,16 @@
+package skillUP_lambda;
+
+import java.util.function.IntSupplier;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		IntSupplier intSupplier = () -> {
+			int num = (int) (Math.random() * 6) + 1;
+			return num;
+		};
+		
+		int num = intSupplier.getAsInt();
+		System.out.println("눈의 수 : " + num);
+	}
+}
