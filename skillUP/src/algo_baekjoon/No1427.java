59a9fbfd2d4a0fc37ec066d049a1fdc9c7580d58
LJH\JH <hwinnovation@naver.com>
2018-11-26 00:41:27 +0900

    1427 ing

---
+++ skillUP/src/algo_baekjoon/No1427.java
@@ -0,0 +1,16 @@
+package algo_baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No1427 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		
+		
+	}
+}
