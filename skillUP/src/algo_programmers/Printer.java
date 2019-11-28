11e4b3dee7a68b4190bb3a2331b05f2f0e4fae1e
LJH\JH <hwinnovation@naver.com>
2019-11-28 23:01:25 +0900

    programmers printer

--- skillUP/src/algo_programmers/Printer.java
+++ skillUP/src/algo_programmers/Printer.java
@@ -1,29 +1,40 @@
 package algo_programmers;
 
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
 public class Printer {
 
     public static void main(String[] args) {
 
+        int[] sample = {2,1,3,2};
+        int location = 2;
+
+        System.out.println(solution(sample, location));
     }
 
-    public int solution(int[] priorities, int location) {
+    public static int solution(int[] priorities, int location) {
+
         int answer = 0;
-        Queue<Integer> q = new LinkedList<>();
-        for(int i : priorities) {
-            q.offer(i);
-        }
-            q[location];
-        if(q.peek() == Collections.max(q)) {
-            q.poll();
-            answer++;
-        } else {
-            q.offer(q.poll());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(priorities.length, Collections.reverseOrder());
+
+        for(int priority : priorities) {
+            pq.offer(priority);
         }
 
+        while(!pq.isEmpty()) {
+
+            for(int i = 0; i < priorities.length; i++) {
+                if(pq.peek() == priorities[i]) {
+                    pq.poll();
+                    answer++;
+                    if (location == i) {
+                        pq.clear();
+                        break;
+                    }
+                }
+            }
+        }
 
         return answer;
     }
