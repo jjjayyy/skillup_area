d2db15180f7546c94776a3de60746499c6eaeeeb
LJH\JH <hwinnovation@naver.com>
2020-04-04 23:57:41 +0900

    programmers solv

--- skillUP/src/algo_programmers/Failure_Rate.java
+++ skillUP/src/algo_programmers/Failure_Rate.java
@@ -1,10 +1,31 @@
 package algo_programmers;
 
+import java.util.Arrays;
+
 public class Failure_Rate {
 
-    public int[] solution(int N, int[] stages) {
+    public static  int[] solution(int N, int[] stages) {
         int[] answer = {};
-        return answer;
+
+        Arrays.sort(stages);
+
+        for(int i = 0; i < stages.length; i++) {
+
+            if(i != 0 && stages[i] != stages[i-1]) {
+                stages[i-1]
+
+                stages[i]
+            }
+            stages[i]
+        }
+
+        return stages;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 1, 2, 6, 2, 4, 3, 3};
+        System.out.println(solution(3, arr));
     }
 
 }
