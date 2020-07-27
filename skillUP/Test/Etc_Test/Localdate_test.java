5e0fc73624595dcce38f352cbabc72787394056e
이진휘 <jhlee89@douzone.com>
2020-07-27 23:37:42 +0900

    add localdate test

--- skillUP/Test/Etc_Test/Localdate_test.java
+++ skillUP/Test/Etc_Test/Localdate_test.java
@@ -1,4 +1,49 @@
 package Etc_Test;
 
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class Localdate_test {
+
+    @Test
+    public void 현재날짜_yymmdd포멧() {
+        LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+        assertThat("20200727", is(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))));
+    }
+
+    @Test
+    public void 날짜_차이() {
+        LocalDate today = LocalDate.now();          //2020-07-27
+        LocalDate targetDay = LocalDate.of(2018, 07, 29);
+
+        Period period = targetDay.until(today);
+
+        assertThat(1, is(period.getYears()));
+        assertThat(11, is(period.getMonths()));
+
+        long month = ChronoUnit.MONTHS.between(targetDay, today);
+        System.out.println(month);
+
+        assertThat(21, is(month));
+    }
+
+    @Test
+    public void 스트링_파싱() {
+        String string = "20190807";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate date = LocalDate.parse(string, formatter);
+        LocalDate today = LocalDate.now();
+
+        long month = ChronoUnit.MONTHS.between(date, today);
+        System.out.println(month);
+
+    }
 }
