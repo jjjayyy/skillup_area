90e6b120ae9923d5e9647245be1fadf1a27c0d4e
이진휘 <jhlee89@douzone.com>
2020-12-06 22:02:49 +0900

    test

--- skillUP/src/algo_programmers/Push_Keypad.java
+++ skillUP/src/algo_programmers/Push_Keypad.java
@@ -59,7 +59,7 @@ public class Push_Keypad {
     }
 
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5, 4};
         String hand = "right";
 
         System.out.println(solution(numbers, hand));
