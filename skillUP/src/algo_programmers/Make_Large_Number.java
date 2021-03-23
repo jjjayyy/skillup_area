aae4fbc733637bf6bfed2699627f3760ced58d98
jjjayyy <hwinnovation@naver.com>
2021-03-24 00:45:03 +0900

    commit

--- skillUP/src/algo_programmers/Make_Large_Number.java
+++ skillUP/src/algo_programmers/Make_Large_Number.java
@@ -8,9 +8,21 @@ public class Make_Large_Number {
     public static String solution(String number, int k) {
         StringBuilder builder = new StringBuilder(number);
 
-        int resultLength = number.length() - k;
-        while(builder.length() > resultLength) {
-            builder.deleteCharAt(0);
+        int left = 0;
+        int right = number.length() - k;
+        int max = -1;
+        int prevMaxIdx = -1;
+
+        while(builder.length() > right) {
+
+            for(int i = left; i < k; i++) {
+                int num = builder.charAt(i) - '0';
+                if(max < num) {
+                    max = num;
+                    if(prevMaxIdx != -1) builder.deleteCharAt(prevMaxIdx);
+                    prevMaxIdx = i;
+                }
+            }
         }
         return builder.toString();
     }
