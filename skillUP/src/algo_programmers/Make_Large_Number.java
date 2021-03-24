ffbf47662a15557d6788041a303ef5b835e786ee
jhlee89 <jhlee89@douzone.com>
2021-03-24 18:06:11 +0900

    algo programmers commit

--- skillUP/src/algo_programmers/Make_Large_Number.java
+++ skillUP/src/algo_programmers/Make_Large_Number.java
@@ -2,26 +2,40 @@ package algo_programmers;
 
 public class Make_Large_Number {
     public static void main(String[] args) {
-        System.out.println(solution("1924", 2));
+        System.out.println(solution("4177252841", 8));
     }
 
     public static String solution(String number, int k) {
         StringBuilder builder = new StringBuilder(number);
 
         int left = 0;
-        int right = number.length() - k;
-        int max = -1;
-        int prevMaxIdx = -1;
+        int right = k;
+        int prevNum = -1;
 
-        while(builder.length() > right) {
-
-            for(int i = left; i < k; i++) {
+        while(builder.length() > number.length() - k) {
+            left = 0;
+            for(int i = left; i < right; i++) {
                 int num = builder.charAt(i) - '0';
-                if(max < num) {
-                    max = num;
-                    if(prevMaxIdx != -1) builder.deleteCharAt(prevMaxIdx);
-                    prevMaxIdx = i;
+                if(num == 9) {
+
                 }
+                if(prevNum < num) {
+                    if(prevNum != -1 && i > 0) {
+                        builder.deleteCharAt(i-1);
+                        i--;
+                        right--;
+                    }
+                } else {
+                    left = i;
+                }
+                prevNum = num;
+            }
+
+            if(builder.length() > right) {
+                right++;
+            } else {
+                builder.deleteCharAt(builder.length()-1);
+                builder.delete(number.length()-k-1, builder.length()-1);
             }
         }
         return builder.toString();
