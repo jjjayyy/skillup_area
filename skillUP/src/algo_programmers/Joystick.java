2e3a491f598a33012d3f00ced211166bec5aa5a3
이진휘 <jhlee89@douzone.com>
2021-04-06 00:08:02 +0900

    commit at home

--- skillUP/src/algo_programmers/Joystick.java
+++ skillUP/src/algo_programmers/Joystick.java
@@ -2,9 +2,12 @@ package algo_programmers;
 
 public class Joystick {
     public static void main(String[] args) {
-        System.out.println(solution("BBAABAAAAB"));
+        System.out.println(solution("AAAZAAZA"));
     }
 
+    //연속으로 A가 많이 들어가있는 부분에서 마지막 A의 index를 가져옴
+    //A가 가장 많이 들어있는 부분을 A블럭이라고 하면 A블럭 앞의 index 구함
+    //전체 길이에서 A블럭 제외하고 A블럭 기준 앞블럭 or 뒷블럭 중 작은 블럭에서 한번 더 더함 (찍고 돌아오는 경우)
     static int solution(String name) {
         int cnt = 0;
         int wordCnt = 0;
@@ -12,6 +15,7 @@ public class Joystick {
         int cntA = 0;
         int tempA = 1;
         boolean isPrevA = false;
+        boolean isAllA = true;
         int idxA = 0;
 
         for(int i = 0; i < name.length(); i++) {
@@ -26,27 +30,23 @@ public class Joystick {
                 continue;
             }
             tempA = 1;
+            isAllA = false;
             isPrevA = false;
             wordCnt = name.charAt(i)-'A';
             cnt += Math.min(wordCnt, wordCntAll-wordCnt);
         }
 
-        int minCount = name.length()-1; //기본 이동거리는 길이 - 1
+        int minCount = name.length()-1;
         int startIdx = idxA - cntA;  // 연속된 A가 가장 많은거 이전 블럭
         int endIdx = minCount - idxA; // 연속된 A가 가장 많은거 이후 블럭
 
         if(cntA != 0) {
             if(startIdx > endIdx) {
-                minCount = minCount - cntA + endIdx;
-            }
-            minCount -
-        }
-
-        for(int i = 0; i < name.length(); i++) {
-            if(cntA != 0) {
-
+                minCount = Math.min(minCount, (minCount - cntA + endIdx));
+            } else {
+                minCount = Math.min(minCount, (minCount - cntA + startIdx));
             }
         }
-        return cnt + minCount;
+        return isAllA ? 0 : cnt + minCount;
     }
 }
