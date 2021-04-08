74ceb348f839b9848c32ac1bc5fbe8d8e064f4b8
jhlee89 <jhlee89@douzone.com>
2021-04-08 18:04:46 +0900

    algo programmers commit

--- skillUP/src/algo_baekjoon/Main.java
+++ skillUP/src/algo_baekjoon/Main.java
@@ -4,9 +4,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //int num1 = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
+        int firstTemp = 0;
+        int secondTemp = 1;
+
+        for(int i = 0; i < n; i++) {
+            int
+            fibonacci(firstTemp,secondTemp);
+        }
+
+
+
+    }
+
+    static int fibonacci(int first, int second) {
+        return first + second;
     }
 }
