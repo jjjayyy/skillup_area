42cb4ea0515738130ac80bfeda4e580f684a46c2
jjjayyy <hwinnovation@naver.com>
2021-08-08 23:55:35 +0900

    commit at home

---
+++ skillUP/src/algo_programmers/KAKAO_2021_Menu_Renewal.java
@@ -0,0 +1,19 @@
+package algo_programmers;
+
+public class KAKAO_2021_Menu_Renewal {
+
+    public static void main(String[] args) {
+        String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+        int[] course = {2,3,4};
+        solution(orders, course);
+    }
+
+    public static String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+
+        // 주문한 사람의 메뉴별 course별 조합구하기
+        // 각 조합별 course 카운팅
+
+        return answer;
+    }
+}
