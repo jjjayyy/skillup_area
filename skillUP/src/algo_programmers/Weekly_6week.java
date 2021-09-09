f8818c3e365f0c5a3d27d7ee3ecf7f34a4d5c962
jh <hwinnovation@naver.com>
2021-09-10 01:04:16 +0900

    skillup by mac

--- skillUP/src/algo_programmers/Weekly_6week.java
+++ skillUP/src/algo_programmers/Weekly_6week.java
@@ -1,29 +1,77 @@
 package algo_programmers;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Comparator.reverseOrder;
+
 public class Weekly_6week {
+
+    public static class Boxer{
+        double winRt;
+        int winCnt;
+        int weight;
+        int num;
+
+        Boxer(double winRt, int winCnt, int weight, int num) {
+            this.winRt = winRt;
+            this.winCnt = winCnt;
+            this.weight = weight;
+            this.num = num;
+        }
+
+        public double getWinRt() {
+            return winRt;
+        }
+
+        public int getWinCnt() {
+            return winCnt;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public int getNum() {
+            return num;
+        }
+    }
 
     public static void main(String[] args) {
 
-        int[] weights = {50, 82, 75, 120};
+        int[] weights = {50,82,75,120};
         String[] head2head = {"NLWL","WNLL","LWNW","WWLN"};
 
         solution(weights, head2head);
     }
 
     public static int[] solution(int[] weights, String[] head2head) {
-        int[] answer = {};
-
-        double[] percentage = new double[weights.length];
+        List<Boxer> list = new ArrayList<>();
         for(int i = 0; i < head2head.length; i++) {
+
             String temp = head2head[i].replace("W", "");
-            double winCnt = head2head[i].length() - temp.length();
-            percentage[i] =  Math.floor(winCnt / (head2head.length-1) * 10000)/100;
+            double cnt = head2head[i].length() - temp.length();
+            int totalFight = head2head[i].replace("N", "").length();
+            double winRt =  totalFight == 0 ? 0 : cnt / totalFight;
+            int winCnt = 0;
+
+            for(int j = 0; j < head2head[i].length(); j++) {
+                if(head2head[i].charAt(j) == 'W' && weights[i] < weights[j]) {
+                    winCnt++;
+                }
+            }
+            list.add(new Boxer(winRt, winCnt, weights[i], i+1));
         }
 
-        for(double per : percentage) {
-            System.out.println(per);
-        }
+        list.sort(Comparator.comparing(Boxer::getWinRt, reverseOrder())
+                .thenComparing(Boxer::getWinCnt, reverseOrder())
+                .thenComparing(Boxer::getWeight, reverseOrder())
+                .thenComparing(Boxer::getNum));
 
-        return answer;
+        for(Boxer boxer : list) {
+            System.out.println(boxer.getNum());
+        }
+        return list.stream().map(Boxer::getNum).mapToInt(i -> i).toArray();
     }
 }
