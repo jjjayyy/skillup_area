7dd4967a41c1c062437a9e9269c61efcac07625c
jjjayyy <hwinnovation@naver.com>
2021-11-29 01:28:27 +0900

    commit at home

--- skillUP/src/algo_baekjoon/No1662.java
+++ skillUP/src/algo_baekjoon/No1662.java
@@ -15,41 +15,47 @@ public class No1662 {
         StringBuilder builder = new StringBuilder();
 
         int length = 0;
+        int tempLength = 0;
+        int cnt = 0;
 
         for(int i = 0; i < S.length(); i++) {
             if(S.charAt(i) == '(') {
                 stack.push(builder.toString());
                 builder.setLength(0);
-
+                cnt++;
                 continue;
             }
 
             if(S.charAt(i) == ')') {
-                if(builder.length() > 0)
-                stack.push(builder.toString());
+                cnt--;
+                if(builder.length() > 0) {
+                    tempLength += builder.length();
 
-                builder.setLength(0);
-
-                if(stack.isEmpty()) {
-                    length += stack.pop().length();
-                } else {
-                    String temp = stack.pop();
-                    if(temp.length() > 0)
-                    length = length * (temp.charAt(temp.length()-1) - '0') + temp.length()-1;
+                    builder.setLength(0);
+                    continue;
+                }
+                if(cnt == 0) {
+                    length += tempLength;
+                    tempLength = 0;
                 }
 
+                String temp = stack.pop();
+                if(temp.length() > 0)
+                length = length * (temp.charAt(temp.length()-1) - '0') + temp.length()-1;
                 continue;
             }
             builder.append(S.charAt(i));
-        }
-        if(builder.length() > 0) {
-            length = builder.length();
         }
 
         if(!stack.isEmpty()) {
             String temp = stack.pop();
             length = length * (temp.charAt(temp.length()-1) - '0') + temp.length()-1;
         }
+
+        if(builder.length() > 0) {
+            length += builder.length();
+        }
+
         System.out.println(length);
     }
 }
