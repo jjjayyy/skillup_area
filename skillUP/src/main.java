78c3d6f1f011a17c9cf3a4a671e82c70644878d7
jjjayyy <hwinnovation@naver.com>
2024-04-04 23:34:04 +0900

    programmers start

--- skillUP/src/main.java
+++ skillUP/src/main.java
@@ -94,6 +94,26 @@ public class main {
 //        solution13(elements);
 
 //        solution16("abracadabra");
+
+        solution17("z", "a", 1);
+    }
+
+    public static String solution17(String s, String skip, int index) {
+        String answer = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            for (int j = 0; j < index; j++) {
+                c += 1;
+                if (c > 'z') {
+                    c -= 26;
+                }
+                if (skip.contains(String.valueOf(c))) {
+                    j--;
+                }
+            }
+            answer += c;
+        }
+        return answer;
     }
 
     private static int cnt = 0;
