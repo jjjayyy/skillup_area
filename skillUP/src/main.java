76c820585c23ef8ab62b31226cb5346eba1e4382
jjjayyy <hwinnovation@naver.com>
2024-06-02 00:38:48 +0900

    leetcode start

--- skillUP/src/main.java
+++ skillUP/src/main.java
@@ -147,7 +147,26 @@ public class main {
         int[] nums2 = {2,5,6};
         int m = 3;
         int n = 3;
-        merge(nums1, m, nums2, n);
+//        merge(nums1, m, nums2, n);
+        Map<String, Integer> s = new HashMap();
+        System.out.println(s.get("key"));
+    }
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> magazineMap = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            magazineMap.put(c, magazineMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (!magazineMap.containsKey(c) || magazineMap.get(c) == 0) {
+                return false;
+            }
+            magazineMap.put(c, magazineMap.get(c) - 1);
+        }
+
+        return true;
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
