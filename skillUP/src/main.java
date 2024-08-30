360f8fdd9273077938d91ddd43702f6c293435fb
jjjayyy <hwinnovation@naver.com>
2024-08-31 01:00:41 +0900

    leetcode start

--- skillUP/src/main.java
+++ skillUP/src/main.java
@@ -165,9 +165,23 @@ public class main {
 //        romanToInt("III");
 //        strStr("sadbutsad", "sad");
 
-        isHappy(2);
+//        isHappy(2);
+
+        int[] nums = {1,0,1,1};
+        containsNearbyDuplicate(nums, 1);
     }
 
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i]) && (i - map.get(nums[i])) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
 
     public static boolean isHappy(int n) {
         Set<Integer> seenNumbers = new HashSet<>();
